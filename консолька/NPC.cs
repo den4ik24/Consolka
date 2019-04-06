ea6f4a544d3d792954b60a80547f5e44d17a7659
Yurchenko Denis <den4ik24@gmail.com>
2019-04-06 21:10:58 +0300

    готово

--- консолька/NPC.cs
+++ консолька/NPC.cs
@@ -18,9 +18,12 @@ namespace консолька
         public virtual void Talk()
         {
             Console.WriteLine();
-            Console.WriteLine("Здоровеньки булы\n" +
-                " - неприветливо сказал он, не поднимая головы от стола - \n" +
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("- Здоровеньки булы");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" - неприветливо сказал он, не поднимая головы от стола - \n" +
                 "чем он там занимался, не было видно, и Вы решили поздоровавшись тихонько отойти и не мешать ему");
+            Console.ForegroundColor = ConsoleColor.White;
         } 
 
     }
@@ -35,7 +38,9 @@ namespace консолька
         public override void Talk()
         {
             Console.WriteLine();
-            Console.WriteLine("Что ты хочешь узнать ?\nЯ всех и все в округе знаю. За недорого сторгуемся");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("- Что ты хочешь узнать ?\nЯ всех и все в округе знаю. За недорого сторгуемся");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
@@ -49,7 +54,9 @@ namespace консолька
         public override void Talk()
         {
             Console.WriteLine();
-            Console.WriteLine("Добрый день!\nЧто ты хочешь приобрести?\nУ нас в лавке большой выбор разнообразного товара");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("- Добрый день!\nЧто ты хочешь приобрести?\nУ нас в лавке большой выбор разнообразного товара");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
@@ -63,10 +70,18 @@ namespace консолька
         public override void Talk()
         {
             Console.WriteLine();
-            Console.WriteLine("Здорова! \n- сказал кузнец, утирая пот. Он отвлекся от наковальни и уставился на Вас.\n" +
-                "" +
-                " В руке у него был нереально большой молот, казалось что и три человека его не поднимут,\n но кузнец держал его одной рукой!\n" +
-                "У нас лучшие мечи - обратился он к Вам - Или тебе нужно подковать лошадь?");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" - Здорова!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("сказал кузнец, утирая пот со лба. Он отвлекся от наковальни и уставился на Вас.\n" +
+                "В руке у него был нереально большой молот, казалось что и три человека его не поднимут,\nно кузнец держал его одной рукой!");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" - У нас лучшие мечи");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("обратился он к Вам");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" - Или тебе нужно подковать лошадь?");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
@@ -80,7 +95,13 @@ namespace консолька
         public override void Talk()
         {
             Console.WriteLine();
-            Console.WriteLine("Какие травы тебе нужны ? - Спросила  женщина из-за прилавка, заваленного разными корешками");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("- Какие травы тебе нужны ?");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("- Спросила  женщина из-за прилавка, заваленного разными корешками. Сверху свисали связки сушенных мухоморов\n " +
+                "По виду прямо ведьма из сказок - длинный нос, торчащий зуб и прищуренный взгляд.\n" +
+                "Вы не решились ничего брать у нее. А то вдруг в козлёночка обратит? ");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
@@ -94,9 +115,13 @@ namespace консолька
         public override void Talk()
         {
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Вы видите задумчивого рыцаря, около входа в Трактир. Он как раз подходит к своей лошади на привязи рядом. \n" +
-                "И Вы решаете к нему обратиться, но не успеваете. Увидев приближающуюся вашу фигуру он первым начинает разговор:\n" +
-                "Ты знаешь, где можно совершить подвиг ? \n Ведь я не смогу жениться на Жозефине не свершив во имя Её множества подвигов. ");
+                "И Вы решаете к нему обратиться, но не успеваете. Увидев приближающуюся вашу фигуру он первым начинает разговор:");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("- Ты знаешь, где можно совершить подвиг ? \n - Ведь я не смогу жениться на Жозефине не свершив во имя Её множества подвигов.\n" +
+                "Он начинает долго рассказывать про свою даму сердца, и вы сославшись на занятость спешите покинуть его. ");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
@@ -110,7 +135,11 @@ namespace консолька
         public override void Talk()
         {
             Console.WriteLine();
-            Console.WriteLine(" -С дороги, чернь - кричит Дворянин пролетая мимо вас на лошади, окатив Вас брызгами из-под копыт");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" -С дороги, чернь");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" - кричит Дворянин пролетая мимо вас на лошади, окатив Вас брызгами из-под копыт");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
@@ -124,8 +153,11 @@ namespace консолька
         public override void Talk()
         {
             Console.WriteLine();
-            Console.WriteLine(" П-п-ривет. Ты не знаешь какой сегодня день ? \n" +
-                "Вы пожимаете плечами и ускоряете шаг, проходя мимо. Вы понимаете, что добится чего либо путного от него не получится");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" - П-п-ривет. Ты не знаешь какой сегодня день ?");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Вы пожимаете плечами и ускоряете шаг, проходя мимо. Вы понимаете, что добится чего либо путного от него не получится");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
@@ -139,9 +171,13 @@ namespace консолька
         public override void Talk()
         {
             Console.WriteLine();
-            Console.WriteLine(" Здрав буди путник! Не желаешь поесть, выпить и отдохнуть с дороги ? Вы сильно устали после перехода через болота, и не мешало бы и отдохнуть, НО! \n" +
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" - Здрав буди путник! Не желаешь поесть, выпить и отдохнуть с дороги ?");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Вы сильно устали после перехода через болота, и не мешало бы и отдохнуть, НО! \n" +
                 "видя странные бурые пятна на полу трактира и грязный стакан, который Трактирщик протирал засаленным халатом, Вы не решились даже зайти внутрь.\n" +
                 " Не говоря о том, что бы что-либо заказать или остаться переночевать. После такой ночевки неизвестно, останешся ли живым вообще...");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
@@ -155,9 +191,11 @@ namespace консолька
         public override void Talk()
         {
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(" Увидев приближающегося Вас дети бросились врасыпную." +
                 "\n Только один малыш испугавшись застыл на месте, и начал громко верещать.\n" +
                 "Вы же, не желая неприятостей, решили просто отойти от него подальше");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
--- консолька/Program.cs
+++ консолька/Program.cs
@@ -10,56 +10,80 @@ namespace консолька
     {
         static void Main(string[] args)
         {
-            try
+            while (true)
             {
-
-                string exit = "";
-                do
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/");
+                Console.WriteLine();
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Вы оказались в небольшой деревушке на окраине болот. Сразу за домами начинается густая стена непроходимого леса.\n" +
+                      "Вы видите перед собой несколько скошенных домов - это и вся деревня." +
+                      "\nВдоль одной кривой улицы - по видимому главной, т.к. других и не наблюдается - расположены Таверна и магазинчики. " +
+                      "\n Попробуем подойти к одному из виднеющихся на улице людей. ");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|");
+                Console.WriteLine();
+               
+                try
                 {
-                                                       
-                    Console.WriteLine("Вы оказались в небольшой деревушке на окраине болот. Сразу за домами начинается густая стена непроходимого леса.\n" +
-                        "Вы видите перед собой несколько скошенных домов - это и вся деревня." +
-                        "\nВдоль одной кривой улицы - по видимому главной, т.к. других и не наблюдается - расположены Таверна и магазинчики. " +
-                        "\n Попробуем подойти к одному из виднеющихся на улице людей. ");
-                    Console.WriteLine();
-
                     List<NPC> NPCS = new List<NPC>();
                     NPCS.Add(new NPC("Крестьянин, сидящий за столом"));
                     NPCS.Add(new NPCasker("Проныра, грызет семечки возле Таверны. Его маленькие и хитрые глазки так и зыркают по сторонам"));
                     NPCS.Add(new NPCtrader("Торговец, сидит на ступенях мелкой лавки у дороги"));
                     NPCS.Add(new NPCblacksmith("Кузнец, звон металла о металл раздается из кузни"));
-                    NPCS.Add(new NPCherbalist("Травник, его длинный нос торчит над прилавком напротив Таверны "));
+                    NPCS.Add(new NPCherbalist("Травница, её длинный нос торчит над прилавком напротив Таверны "));
                     NPCS.Add(new NPCknight("Рыцарь в ржавых доспехах, выходящий из Таверны"));
                     NPCS.Add(new NPCnobleman("Расфуфыренный Дворянин, скачущий на гнедом жеребце, вдоль улицы по лужам"));
                     NPCS.Add(new NPCdrink("Пьяница, валяющийся за углом Таверны. Похоже завсегдатай заведения"));
                     NPCS.Add(new NPCmaster("Хозяин таверны, протирающий стакан у входа в своё заведение"));
                     NPCS.Add(new NPCchildren("Стайка детей, играющих в зоровенной луже посреди дороги"));
-
+                  
                     int i = 0;
                     foreach (var npc in NPCS)
                     {
-                       
-                        Console.WriteLine( i + " - " + npc.name);
+
+                        Console.WriteLine(i + " - " + npc.name);
                         i++;
-                        
+
                     }
                     Console.WriteLine();
                     Console.WriteLine("К кому вы хотите обратиться?");
-                    NPCS[Convert.ToInt32(Console.ReadLine())].Talk();
+                    string exit = "";
                     exit = Console.ReadLine();
-                }
-                while (exit != "exit");
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine($"Вы ввели не число. {ex.Message}");
-            }
-            finally
-            {
-                Console.ReadKey();
-            }
 
-            
+                    if (exit == "exit")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("конец программы");
+                        Console.ReadKey();
+                        return;
+                    }
+                                         
+                    if (Convert.ToInt32(exit) >= 10 )
+                    {
+                        Console.WriteLine("Вы ввели число, превышающее количество персонажей");
+                        continue;
+                    }
+
+                    NPCS[Convert.ToInt32(exit)].Talk();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Вы ввели не число. {ex.Message}");
+                }
+                finally
+                {
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            }
         }
     }
 }
